6c89886f7ae90837242be7bfb7231cc168fde064
Hr-99 <jhritik0326@gmail.com>
2025-08-07 13:33:10 +0530

    photo upload folder..

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const { startAttendanceCleanupJob } = require('./scheduler');
+const fs = require('fs');
+const path = require('path');
 
 
 require('dotenv').config();
